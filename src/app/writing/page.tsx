001b12e95119504ee05dd1d925ddeae4d478f2b4
Parshvi Jain <152031628+parshvi1508@users.noreply.github.com>
2026-08-31 17:35:53 +0000

    Revert "Updated agent.png"
    This reverts commit 1fdbfb6f62d851618de2cdbbe07f10442bf3c455.

--- src/app/writing/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// No posts exist yet -- redirect to home.
-// When MDX posts are added under /content/posts, render them here.
-export default function WritingPage() {
-  redirect('/')
-}
